0ee2b4d36224cfec988d38a1c1d97f68265202a6
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-07-24 15:48:29 +0530

    Initial Commit

---
+++ swiftUI-Grids/swiftUI_GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_GridsApp.swift
+//  swiftUI-Grids
+//
+//  Created by Jaimin Raval on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
