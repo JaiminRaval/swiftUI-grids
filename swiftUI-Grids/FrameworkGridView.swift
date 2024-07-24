7ac79ae57095878d697cdff8c6b3fc3fd4fce905
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-07-24 17:45:57 +0530

    Populated ui with dummy data

--- swiftUI-Grids/FrameworkGridView.swift
+++ swiftUI-Grids/FrameworkGridView.swift
@@ -8,39 +8,44 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
     let columns: [GridItem] =  [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible()),]
+    
     var body: some View {
-        LazyVGrid(columns: columns){
-            FrameWorkTitleView(name: "app-clip",imageName: "App-Clip")
-            FrameWorkTitleView(name: "app-clip",imageName: "App-Clip")
-            FrameWorkTitleView(name: "app-clip",imageName: "App-Clip")
-            FrameWorkTitleView(name: "app-clip",imageName: "App-Clip")
-            FrameWorkTitleView(name: "app-clip",imageName: "App-Clip")
-            FrameWorkTitleView(name: "app-clip",imageName: "App-Clip")
-            FrameWorkTitleView(name: "app-clip",imageName: "App-Clip")
+        NavigationView {
+            ScrollView{
+                LazyVGrid(columns: columns){
+                    ForEach(MockData.frameworks, id: \.id){ frameWork in
+                        FrameWorkTitleView(frameWork: frameWork)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
         }
     }
 }
 
 #Preview {
     FrameworkGridView()
+        .preferredColorScheme(.dark)
 }
 
 struct FrameWorkTitleView: View {
-    var name: String
-    var imageName: String
+    var frameWork: Framework
+//    var name: String
+//    var imageName: String
     
     var body: some View {
         VStack(spacing: 10) {
-            Image(name)
+            Image(frameWork.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(imageName)
+            Text(frameWork.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
-            
         }
+        .padding()
     }
 }
